d78682c39ade045f7eeecb12a90f91be7efc80b1
Ashish Kushwaha <ashish.kus2408@gmail.com>
2023-02-13 14:38:08 +0530

    Bringing back on track

---
+++ Python/Basic/wrapPattern.py
@@ -0,0 +1,11 @@
+num=int(input("NUM: "))
+for i in range(1, num+1):
+    for j in range(1,i+1):
+        print(j, end="")
+    print("")
+
+for k in range(4, 1, -1):
+    for l in range(1, k+1):
+        print(l, end="")
+    print("")
+
