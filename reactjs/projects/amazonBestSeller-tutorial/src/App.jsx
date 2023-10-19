d4de08cd5ed72cb1a45f4c6429833d0013dcf9ea
ashish-kus <ashish.kus2408@gmail.com>
2023-10-19 19:07:08 +0530

    react

--- reactjs/projects/amazonBestSeller-tutorial/src/App.jsx
+++ reactjs/projects/amazonBestSeller-tutorial/src/App.jsx
@@ -1,52 +1,61 @@
 import './App.css'
+const books = [
+  {
+    author: 'Jordan Moore',
+    id: 123,
+    title: 'Interesting Facts For Curious Minds',
+    img: 'https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL._AC_UL900_SR900,600_.jpg',
+  },
+  {
+    author: 'James Clear',
+    id: 124,
+    title: 'Atomic Habits',
+    img: 'https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL._AC_UL900_SR900,600_.jpg',
+  },
+];
+
+
+// const ExampleEvent = () => {
+//   const handleFormInput = () => {
+//     console.log("input handeller");
+//   };
+//   const handelebtn = () => {
+//     console.log("btn click");
+//   }
+//   return (
+//     <section>
+//       <form onSubmit={handleSubmit}>
+//         <h2>Basic Form</h2>
+//         <input type="txt" name="name" value="Name" onChange={handleFormInput} />
+//         <button type="button" onClick={handelebtn}>BTN</button>
+//       </form>
+//     </section>
+//   )
+// }
 const App = () => {
+  const getBook = (id) => {
+    const book =  book.find((book) => { book.id === id });
+    console.log(book);
+  }
   return (
     <section className='booklist'>
-      
-      <Book/>
-      <Book/>
-      <Book/>
-      <Book/>
-      <Book/>
-      <Book/>
-      <Book/>
-      <Book/>
+      {books.map((book) => {
+        return <Book book={...book} key={book.id} getBook={getBook} />;
+      })}
+    </section>
+  );
+}
+
+const Book = (props) => {
+  const { img, title, author, getBook } = props.book;
+  console.log(props)
+  return (
+    <section className='book'>
+      <img src={img} alt={title} />
+      <h2>{title}</h2>
+      <button  onClick={getBook}>click me</button>
+      <h4>{author} </h4>
     </section>
   )
 }
-
-const Book = () => {
-  return (
-  <section className='book'>
-      <Tittle/>
-      <Auther/>
-      <Image/>
-    </section>
-  )
-}
-
-const Image = () => {
-  return (
-  <>
-    <img src="https://wimpykid.com/wp-content/uploads/2021/10/1-300x500.png" />
-  </>
-  )
-}
-
-const Tittle = () => {
-  return (
-  <>
-    <h1>Image place holder</h1>
-  </>
-  )
-}
-
-const Auther = () => {
-  return (
-  <>
-    <h4>Image place holder</h4>
-  </>
-  )
-}
-
 export default App
